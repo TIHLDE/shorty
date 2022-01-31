c47846e7cab531ccee109d3e5da9f97550de6fff
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-01-31 16:26:55 +0100

    fix(urls): updates to new plural urls

--- fetch/api.tsx
+++ fetch/api.tsx
@@ -3,8 +3,8 @@ import { Event, JobPost, News, Page, ShortLink } from 'types/Types';
 
 export default {
   getEvent: (eventId: number) => IFetch<Event>(`events/${String(eventId)}/`),
-  getJobPost: (id: number) => IFetch<JobPost>(`jobpost/${String(id)}/`),
+  getJobPost: (id: number) => IFetch<JobPost>(`jobposts/${String(id)}/`),
   getNewsItem: (id: number) => IFetch<News>(`news/${String(id)}/`),
-  getPage: (path: string) => IFetch<Page>(`page/${path}`),
-  getShortLink: (slug: string) => IFetch<ShortLink>(`short-link/${slug}/`),
+  getPage: (path: string) => IFetch<Page>(`pages/${path}`),
+  getShortLink: (slug: string) => IFetch<ShortLink>(`short-links/${slug}/`),
 };
