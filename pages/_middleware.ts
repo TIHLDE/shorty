dda2f69db724cd37097d65159f186766affcb484
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-02-16 23:18:39 +0100

    fix

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,33 +1,42 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import URLS, { BASE_URL } from 'URLS';
 import API from 'fetch/api';
 import { sanitizeUrl } from '@braintree/sanitize-url';
+import { sentryCaptureException } from 'utils';
 
 export const middleware = async ({ ua, page }: NextRequest) => {
-  const isBot = ua.isBot;
-  if (isBot) {
-    return NextResponse.next();
-  }
+  try {
+    const isBot = ua.isBot;
+    if (isBot) {
+      return NextResponse.next();
+    }
 
-  const path = page.params.path as unknown as Array<string>;
-  const base = path[0];
-  if (base === 'a') {
-    return NextResponse.redirect(`${BASE_URL}${URLS.events}${path[1]}/`);
-  }
-  if (base === 'k') {
-    return NextResponse.redirect(`${BASE_URL}${URLS.jobposts}${path[1]}/`);
-  }
-  if (base === 'n') {
-    return NextResponse.redirect(`${BASE_URL}${URLS.news}${path[1]}/`);
-  }
-  if (base === 'om' || base === 'wiki') {
-    return NextResponse.redirect(`${BASE_URL}${URLS.wiki}${path.slice(1).join('/')}/`);
-  }
+    const path = (page.params.path || []) as unknown as Array<string>;
+    const base = path[0];
+    if (base) {
+      if (base === 'a') {
+        return NextResponse.redirect(`${BASE_URL}${URLS.events}${path[1]}/`);
+      }
+      if (base === 'k') {
+        return NextResponse.redirect(`${BASE_URL}${URLS.jobposts}${path[1]}/`);
+      }
+      if (base === 'n') {
+        return NextResponse.redirect(`${BASE_URL}${URLS.news}${path[1]}/`);
+      }
+      if (base === 'om' || base === 'wiki') {
+        return NextResponse.redirect(`${BASE_URL}${URLS.wiki}${path.slice(1).join('/')}/`);
+      }
 
-  const shortLink = await API.getShortLink(path.join('/'));
-  if (shortLink) {
-    return NextResponse.redirect(sanitizeUrl(shortLink.url));
+      const shortLink = await API.getShortLink(path.join('/'));
+      if (shortLink) {
+        return NextResponse.redirect(sanitizeUrl(shortLink.url));
+      }
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      await sentryCaptureException(e);
+    }
   }
   return NextResponse.redirect(BASE_URL);
 };
